2fd1091536a2970f96f87c8260c09a24c696dd25
romirbulbule <romir.bulbule@gmail.com>
2025-09-30 17:54:08 -0700

    I think this time its fully fixed and completely independent of those taskfeedview and taskfeedviewmodel swift files, because i completely deleted those files and added a new file to call for taskposts on mainfeedview.

---
+++ TaskMutual/TaskPost.swift
@@ -0,0 +1,35 @@
+//
+//  TaskPost.swift
+//  TaskMutual
+//
+//  Created by Romir Bulbule on 9/30/25.
+//
+
+// If you want a separate file (TaskPost.swift)
+
+
+import Foundation
+
+struct TaskPost: Identifiable {
+    var id: UUID
+    var title: String
+    var description: String
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
